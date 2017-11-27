75dd2af26e5aff026d0cc580405b5cac726cb7fc
urbangregorc <gregorcurban@gmail.com>
2017-11-27 10:27:43 +0100

    I included vsnspi_new, changed Spi type to vsnSPI_CommonStructure and aded Gpio_t for analog switch on SNP

--- hardware/src/boards/VESNA/spi-board.h
+++ hardware/src/boards/VESNA/spi-board.h
@@ -14,17 +14,18 @@ Maintainer: Miguel Luis and Gregory Cristian
 */
 #ifndef __SPI_MCU_H__
 #define __SPI_MCU_H__
-
+#include "vsnspi_new.h"
 /*!
  * SPI driver structure definition
  */
 struct Spi_s
 {
-    SPI_HandleTypeDef Spi;
+	vsnSPI_CommonStructure Spi;
     Gpio_t Mosi;
     Gpio_t Miso;
     Gpio_t Sclk;
     Gpio_t Nss;
+    Gpio_t Switch;
 };
 
 #endif  // __SPI_MCU_H__
